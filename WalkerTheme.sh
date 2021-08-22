b29768bd4e0619a983f3737c2cb5d3041a02e5b6
WalkerG15 <89352705+WalkerG15@users.noreply.github.com>
2021-08-23 02:18:53 +0500

    Add files via upload

---
+++ WalkerTheme.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#coded by Baje Tech/080HACKERS
+#Created On 10/02/2021 In Nigeria By Baje Tech Kanuri Boy
+#If You Take Money For This Little Tool Bash Script Please Look At Your Self Again And Shame To Your
+clear
+echo ""
+echo " Welcome Soldier... Join Walkers =)"|lolcat
+
+echo   "                                            "|lolcat
+echo   "░█──░█ █▀▀█ █── █─█ █▀▀ █▀▀█"|lolcat
+echo   "░█░█░█ █▄▄█ █── █▀▄ █▀▀ █▄▄▀"|lolcat
+echo   "░█▄▀▄█ ▀──▀ ▀▀▀ ▀─▀ ▀▀▀ ▀─▀▀"|lolcat
+echo "     [Coded By Walker (Anonymous) =/]"|lolcat
+sleep 1.0
+echo ""
+echo "This Will Change Your Termux Home Theme Color To Look Aweasome"|lolcat
+echo ""
+read -p 'Please Press Enter To Continue Or Press CTRL + Z To Cancel'
+echo ""
+sleep 0.0
+echo "changing termux theme..."|lolcat
+sleep 2.0
+echo " Please Wait For 10 Seconds To Make The Change..."|lolcat
+cd
+cd /$HOME
+rm //data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+mv /data/data/com.termux/files/home/WalkerTheme/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
+sleep 10.0
+echo "Finished, Your Termux Now Looking Aweasome"|lolcate
+sleep 1.0
+echo "</> learn your own programming lang!"|lolcate
+echo "In Other To Let You See The Changes"|lolcate
+sleep 1.0
+echo "Exiting... Please Relounch The Termux Again"|lolcat
+echo "Exit From Termux And Open It Again To Se The Changes"|lolcat
